60afd6018b811aae35163d7c49e3e7005d429fed
voyccom <john@hagstrand.com>
2020-08-25 06:10:09 -0700

    first pass at fixrel

--- python/fixrel.py
+++ python/fixrel.py
@@ -1,25 +1,81 @@
-#replace pkey with defid
-#loop thru the rel table
-#for each rel record
-#	get the def record matching pkey1
-#	get the id from that record
-#	set it in wn.rel.defid1
-#
-#	get the def record matching pkey2
-#	get the id from that record
-#	set it in wn.rel.defid2
-#
-#	update the rel record
-#
-# =+  | Derivationally related form  | lexical | nv
-# ^   | Also see                     | lexical | vj
-# \   | Pertainym (pertains to noun) | lexical | j
-# -\  | Pertainym (derived from adj) | lexical | a
-# <   | Participle of verb           | lexical | j
+# fixrel.py
+# set defid1 and defid2 in wn.rel
 
-lexptrs = ['=+','^','\\','-\\','<']
-ptr = 'h'
-print(ptr in lexptrs)
-ptr = '^'
-print(ptr in lexptrs)
+import json
+import psycopg2
 
+# open config settings
+dircfg = '../../'
+fconfig = open(f'{dircfg}/config.json', 'r')
+config = json.load(fconfig)
+print(config['default']['appname_long'])
+
+# connect to database
+gconn = psycopg2.connect(f"dbname={config['db']['name']} user={config['db']['user']} password={config['db']['password']} port={config['db']['port']}") 
+gconn.autocommit = True
+
+def fixpkey(pkeyin):
+	#a0000174001
+	if pkeyin[10:12] == '00':
+		pkeyout = pkeyin[0:10]
+	else:
+		pkeyout = pkeyin
+	return pkeyout
+
+def lookup(pkey):
+	global gconn
+	cur = gconn.cursor()
+	sql = 'select id from wn.def where pkey = %s'
+	cur.execute(sql, (pkey,))
+	defid = cur.fetchone()
+	cur.close()
+	return defid
+
+def update(num,defid,pkey):
+	global gconn
+	if num == 1:
+		sql = 'update wn.rel set defid1 = %s where pkey1 = %s'
+	elif num == 2:
+		sql = 'update wn.rel set defid2 = %s where pkey2 = %s'
+	cur = gconn.cursor()
+	cur.execute(sql, (defid,pkey))
+	cur.close()
+
+counter = 0
+runaway = 400000
+
+# pass 1
+sqlin = 'select distinct pkey1 from wn.rel'
+sqlin += ' where defid1 is null order by pkey1'
+curin = gconn.cursor()
+curin.execute(sqlin)
+for row in curin:
+	counter += 1
+	if counter > runaway:
+		break;
+	pkey = row[0]
+	pkey = fixpkey(pkey) 
+	defid = lookup(pkey)
+	update(1,defid,pkey)
+	if counter%1000 == 0:
+		print(f'{counter},', end='', flush=True)
+curin.close()
+
+# pass 2
+counter = 0
+sqlin = 'select distinct pkey2 from wn.rel'
+sqlin += ' where defid2 is null order by pkey2'
+curin = gconn.cursor()
+curin.execute(sqlin)
+for row in curin:
+	counter += 1
+	if counter > runaway:
+		break;
+	pkey = row[0]
+	pkey = fixpkey(pkey) 
+	defid = lookup(pkey)
+	update(2,defid,pkey)
+	if counter%1000 == 0:
+		print(f'{counter},', end='', flush=True)
+curin.close()
+print('complete')
